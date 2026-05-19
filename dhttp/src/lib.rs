884ab12f2acf698b2946d6c72e6365e03d83582f
eareimu <reimu@genmeta.net>
2026-05-19 20:45:34 +0800

    feat: expose identity modules

--- dhttp/src/lib.rs
+++ dhttp/src/lib.rs
@@ -1,5 +1,11 @@
 pub mod ddns;
 pub mod endpoint;
+pub mod identity {
+    pub use dhttp_identity::identity::*;
+}
+pub mod name {
+    pub use dhttp_identity::name::*;
+}
 pub mod trust;
 
 pub use h3x;
